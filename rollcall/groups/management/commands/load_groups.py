3466f10095f295a4833e3c18a10f525d384e5477
Mark Norman Francis <norm@cackhanded.net>
2013-12-29 11:30:24 +0000

    Management command to import groups from Google

---
+++ rollcall/groups/management/commands/load_groups.py
@@ -0,0 +1,55 @@
+import httplib2
+
+from apiclient.discovery import build
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
+from oauth2client.tools import run
+
+from django.core.management.base import BaseCommand
+
+from rollcall.groups.models import Group
+from rollcall.people.models import Person
+
+
+
+class Command(BaseCommand):
+    args = 'args'
+    help = 'help'
+
+    def handle(self, *args, **options):
+        flow = flow_from_clientsecrets(
+            'client_secrets.json',
+            scope='https://www.googleapis.com/auth/admin.directory.group.readonly',
+            message='oops',
+        )
+        storage = Storage('tokens.dat')
+        credentials = storage.get()
+        if credentials is None or credentials.invalid:
+            credentials = run(flow, storage)
+
+        http = httplib2.Http()
+        http = credentials.authorize(http)
+
+        service = build('admin', 'directory_v1', http=http)
+
+        fetch_more = True
+        page_token = None
+        while fetch_more:
+            list_groups = service.groups().list(
+                    domain = 'digital.cabinet-office.gov.uk',
+                    pageToken = page_token,
+                ).execute()
+
+            page_token = list_groups.get('nextPageToken', None)
+            if page_token is None:
+                fetch_more = False
+
+            for item in list_groups['groups']:
+                group, created = Group.objects.get_or_create(
+                    name = item['name'],
+                    email = item['email'],
+                    description = item['description'],
+                )
+                if created:
+                    group.save()
+                    print 'Added', item['name']
