9f5f2c3cf3955df42575437e6a7f698dc6e4a1ca
Mark Norman Francis <norm@cackhanded.net>
2014-01-09 11:10:51 +0000

    Show gravatar on person page

--- rollcall/people/models.py
+++ rollcall/people/models.py
@@ -1,3 +1,5 @@
+import md5
+
 from django.db import models
 
 from autoslug import AutoSlugField
@@ -9,6 +11,10 @@ class Person(models.Model):
     email = models.CharField( max_length=256, unique=True )
     google_id = models.CharField( max_length=64 )
     slug = AutoSlugField( populate_from='name', unique=True )
+
+    @property
+    def avatar_url(self):
+        return 'http://gravatar.com/avatar/%s' % md5.new(self.email).hexdigest()
 
     def __unicode__(self):
         return self.name
