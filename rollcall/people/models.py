2e68ffdec67edaaea2a805f25e0dbbaab411cd26
Mark Norman Francis <norm@cackhanded.net>
2014-01-09 12:29:26 +0000

    Show clan on person page

--- rollcall/people/models.py
+++ rollcall/people/models.py
@@ -17,21 +17,30 @@ class Person(models.Model):
         return 'http://gravatar.com/avatar/%s' % md5.new(self.email).hexdigest()
 
     def manager(self):
-        for membership in self.membership_set.filter(role='MEMBER'):
+        for membership in self.memberships_as_member():
             if membership.group.email.find('gds-reports') == 0:
                 first_owner = membership.group.owners()[0]
                 return first_owner.person
         return None
 
     def role(self):
-        for membership in self.membership_set.filter(role='MEMBER'):
+        for membership in self.memberships_as_member():
             if membership.group.email.find('gds-role') == 0:
+                return membership.group.name
+
+    @property
+    def clan(self):
+        for membership in self.membership_set.all():
+            if membership.group.email.find('gds-clan') == 0:
                 return membership.group.name
 
     def memberships(self):
         return self.membership_set.all()
 
-    def owns(self):
+    def memberships_as_member(self):
+        return self.membership_set.filter(role='MEMBER')
+
+    def memberships_as_owner(self):
         return self.membership_set.filter(role='OWNER')
 
     def __unicode__(self):
